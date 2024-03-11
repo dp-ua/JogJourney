5ba1e64cfaf62cdb3762dddadaacc6e089435b3d
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-03-11 15:47:36 +0200

    [refactoring] remove useless entities and repo-services

--- src/main/java/com/dp_ua/JogJourney/dba/repo/HeatLineRepo.java
+++
@@ -1,9 +0,0 @@
-package com.dp_ua.JogJourney.dba.repo;
-
-import com.dp_ua.JogJourney.dba.element.HeatLineEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HeatLineRepo extends CrudRepository<HeatLineEntity, Long> {
-}
--- src/main/java/com/dp_ua/JogJourney/dba/repo/HeatRepo.java
+++
@@ -1,9 +0,0 @@
-package com.dp_ua.JogJourney.dba.repo;
-
-import com.dp_ua.JogJourney.dba.element.HeatEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HeatRepo extends CrudRepository<HeatEntity, Long> {
-}
