9239bd2571b9550c338bc35023bb3bf7b82148fd
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-03-12 20:25:17 +0200

    [refactoring] fix warnings

--- src/main/java/com/dp_ua/JogJourney/exception/StravaApiException.java
+++ src/main/java/com/dp_ua/JogJourney/exception/StravaApiException.java
@@ -1,10 +1,13 @@
 package com.dp_ua.JogJourney.exception;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
+@Getter
+@Slf4j
 public class StravaApiException extends Exception {
     @Getter
-    private long code;
+    private final long code;
     //  401 - Unauthorized
     //  403 - Forbidden
     //  404 - Not Found
